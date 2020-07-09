1f778dbb10c7310152a020ece08e95ee2dada6d5
jennifergraceprince <github email address>
2020-07-09 04:22:14 -0700

    test

---
+++ spec/learn-about-dogs_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LearnAboutDogs do
+  it "has a version number" do
+    expect(LearnAboutDogs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
