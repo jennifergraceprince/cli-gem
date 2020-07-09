faa51a0f538f32ad0849839ad32d35c68152db45
jennifergraceprince <github email address>
2020-07-09 03:21:35 -0700

    dependencies

---
+++ lib/learn_about_dogs.rb
@@ -0,0 +1,9 @@
+require "pry"
+require 'nokogiri'
+require "open-uri"
+
+require "dog_breeds/version"
+
+require_relative "./dog_breeds/cli.rb"
+require_relative "./dog_breeds/scraper.rb"
+require_relative "./dog_breeds/dogs.rb"
