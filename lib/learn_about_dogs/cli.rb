266e230f29ae853bfa386996fb4257e0a7d8234c
jennifergraceprince <github email address>
2020-07-10 10:16:00 -0700

    cli.rb cleanup

--- lib/learn_about_dogs/cli.rb
+++ lib/learn_about_dogs/cli.rb
@@ -15,12 +15,12 @@ class LearnAboutDogs::CLI
 		puts "----------------------------------------"
 		breeds = make_dogs
 		@i = 0
-		@j = 9
+		@d = 9
 		list_dogs(breeds)
 	end
 
 	def make_dogs #Scrapes site index page to gather all of the breeds and breed urls
-		breeds_array = LearnAboutDogs::Scraper.scrape_index(BASE_PATH + "/dogs/breeds")
+		breeds_array = LearnAboutDogs::Scraper.scrape_index(BASE_PATH + "/cats/breeds")
 		breeds_array.collect do |breed|
 			LearnAboutDogs::Dog.new(breed[:name], breed[:page_url])
 		end
@@ -28,16 +28,20 @@ class LearnAboutDogs::CLI
 
 	def list_dogs(breeds) #indexes the array of dog breeds returned from make_dogs and lists each one for user to select from
 		puts ""
-		breeds[@i..@i+@j].each.with_index(@i + 1) {|b,i|puts "[#{i}] #{b.name}"}
-		puts "[all]" if @j != 243
-		puts "[less]" if @j == 243
-		puts "[next]" if @i == 0 && @j == 9
-		puts "[back||next]" if @i >= 10 && @i+@j <243
-		puts "[back]" if @i+@j >= 243 && @j == 9
+		breeds[@i..@i+@d].each.with_index(@i + 1) {|b,i|puts "#{i} - #{b.name}"}
+		puts "<- all ->" if @d != 243
+		puts "<- less  " if @d == 243
+		puts "  next ->" if @i == 0 && @d == 9
+		puts "<- previous || next ->" if @i >= 10 && @i+@d <243
+		puts "<- previous  " if @i+@d >= 243 && @d == 9
 		puts ""
-		puts "type [exit] at any time to close"
+		puts "type ALL to see the full list."
+		puts "type LESS from the full list to return to the truncated list."
+		puts "type NEXT to page through the list 10 at a time."
+		puts "type PREVIOUS to return to the preview view."
+		puts "type EXIT at any time to close the program."
 		puts ""
-		puts "Enter the dog breed name or menu number you want to learn about:"
+		puts "Enter the NAME or MENU NUMBER of a breed you'd like to learn about:"
 		input = gets.strip
 		if input.to_i > 0 && input.to_i <= breeds.length
 			view_breed_overview(LearnAboutDogs::Dog.all[input.to_i - 1])
@@ -45,24 +49,24 @@ class LearnAboutDogs::CLI
 			view_breed_overview(LearnAboutDogs::Dog.all.detect{|breed| breed.name.downcase == input.downcase})
 		elsif input.downcase == "all"
 			@i = 0
-			@j = 243
+			@d = 243
 			list_dogs(breeds)
 		elsif input.downcase == "less"
 			@i = 0
-			@j = 9
+			@d = 9
 			list_dogs(breeds)
-		elsif input.downcase == "next" && @i+@j == 243
+		elsif input.downcase == "next" && @i+@d == 243
 			puts ""
-			puts "That's every dog breed!"
+			puts "That's the whole list!"
 			list_dogs(breeds)
 		elsif input.downcase == "next"
 			@i += 10
 			list_dogs(breeds)
-		elsif input.downcase == "back" && @i == 0
+		elsif input.downcase == "previous" && @i == 0
 			puts ""
-			puts "That's every dog breed!"
+			puts "That's the whole list!"
 			list_dogs(breeds)
-		elsif input.downcase == "back"
+		elsif input.downcase == "previous"
 			@i -= 10
 			list_dogs(breeds)
 		elsif input.downcase == "exit"
@@ -74,31 +78,31 @@ class LearnAboutDogs::CLI
 		end
 	end
 
-	def view_breed_overview(breed) #upon selecting a specific dog breed, this method scrapes that breed's url page for more information
+	def view_breed_overview(breed) #when choosing a breed, this method scrapes that breed's url page for more information
 		details = LearnAboutDogs::Scraper.scrape_profile(BASE_PATH + breed.page_url)
 		breed.add_details(details)
 		puts ""
 		puts "----------------------------------------"
-		puts "Overview of the #{breed.name}"
+		puts "The #{breed.name}"
 		puts "----------------------------------------"
 		puts ""
 		puts "#{breed.blurb}"
 		puts ""
-		puts "Fun Fact!"
+		puts "Did you know?"
 		puts "#{breed.fun_fact}"
 		view_more_details(breed)
 	end
 
 	def view_more_details(breed) #after more info is scraped from view_breed_overview, more options are presented to learn more about breed
 		puts ""
-		puts "Learn more about the #{breed.name}:"
-		puts "[1] Description"
-		puts "[2] Characteristics"
-		puts "[3] History"
-		puts "[4] Personality"
-		puts "[5] Grooming"
-		puts "[6] Health"
-		puts "[Back] to list of all dog breeds"
+		puts "Continue learning about the #{breed.name}:"
+		puts "1 - Description"
+		puts "2 - Characteristics"
+		puts "3 - History"
+		puts "4 - Personality"
+		puts "5 - Grooming"
+		puts "6 - Health"
+		puts "To search another breed or return to main menu, type PREVIOUS."
 		input = gets.strip
 		topic = nil
 		info = nil
@@ -127,7 +131,7 @@ class LearnAboutDogs::CLI
 		when "6","health"
 			topic = "Health"
 			info = breed.health
-		when "back"
+		when "previous"
 			start
 		when "exit"
 			goodbye
@@ -152,8 +156,8 @@ class LearnAboutDogs::CLI
 			info.call
 		end
 		puts ""
-		puts "[1] Learn more about the #{breed.name}"
-		puts "[2] Learn about a different breed of dog."
+		puts "1 - See details about the #{breed.name}"
+		puts "2 - Return to previous menu."
 		input = gets.strip
 		case input.downcase
 		when "1"
